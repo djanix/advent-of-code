8795e2e3069cee0bba57ec41cf5b6ead38f8291a
Janic Beauchemin <jbeauchemin33@gmail.com>
2024-12-21 23:32:57 -0500

    Exercise 13 refactor

--- 2024/13/13_1.ts
+++ 2024/13/13_1.ts
@@ -4,21 +4,26 @@ const data = readFileSync('input', 'utf8').split('\n');
 let total = 0;
 
 for (let i = 0; i < data.length; i+=4) {
-  const [buttonAx, buttonAy] = data[i].match(/\d+/g);
-  const [buttonBx, buttonBy] = data[i + 1].match(/\d+/g);
-  const [prizePositionX, prizePositionY] = data[i + 2].match(/\d+/g);
+  const [ax, ay] = data[i].match(/\d+/g);
+  const [buttonAx, buttonAy] = [ax, ay].map(Number);
+
+  const [bx, by] = data[i + 1].match(/\d+/g);
+  const [buttonBx, buttonBy] = [bx, by].map(Number);
+
+  const [px, py] = data[i + 2].match(/\d+/g);
+  const [prizePositionX, prizePositionY] = [px, py].map(Number);
 
   let minTokens = 0;
 
-  for (let x = 0; x <= Number(prizePositionX); x += Number(buttonAx)) {
-    if ((Number(prizePositionX) - x) % Number(buttonBx) !== 0) continue;
+  for (let x = 0; x <= prizePositionX; x += buttonAx) {
+    if ((prizePositionX - x) % buttonBx !== 0) continue;
 
-    const aPress = x / Number(buttonAx);
-    const bPress = (Number(prizePositionX) - x) / Number(buttonBx);
+    const aPress = x / buttonAx;
+    const bPress = (prizePositionX - x) / buttonBx;
 
     if (aPress > 100 || bPress > 100) continue;
 
-    if ((aPress * Number(buttonAy)) + (bPress * Number(buttonBy)) === Number(prizePositionY)) {
+    if ((aPress * buttonAy) + (bPress * buttonBy) === prizePositionY) {
       const tokenCount = (aPress * 3) + bPress;
       if (!minTokens || tokenCount < minTokens) minTokens = tokenCount;
     }
